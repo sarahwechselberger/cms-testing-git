7887fa3c3086b920eb03f2290d36be5fec97d613
sarahwechselberger <sarah.wechselberger@wh-d.at>
2026-09-15 14:17:13 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,26 +3,57 @@ import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
     stackbitVersion: '~0.6.0',
+
     ssgName: 'eleventy',
+
+    // Eleventy dev server used by Netlify Visual Editor
+    devCommand: 'npx @11ty/eleventy --serve --port {PORT}',
+
+    // Eleventy-specific Visual Editor configuration
+    experimental: {
+        ssg: {
+            proxyWebsockets: true,
+
+            logPatterns: {
+                up: ['Server at'],
+            },
+        },
+    },
+
+    // Let Eleventy handle content reloads
+    customContentReload: true,
 
     contentSources: [
         new GitContentSource({
             rootPath: process.cwd(),
-            contentDirs: ["content"],
+
+            contentDirs: ['content'],
+
             models: [
                 {
                     name: 'page',
                     type: 'page',
-                    filePath: 'content/pages/{slug}.md',
-                    urlPath: '/{slug}/',
-                    "fields": [
-                        { name: "title", type: "string", required: true },
-                        { name: "slug", type: "slug" },
-                        { name: "markdown_content", type: "markdown" }
-                    ]
-                }
-            ]
-        })
-    ]
 
+                    filePath: 'content/pages/{slug}.md',
+                    urlPath: '/{slug}',
+
+                    fields: [
+                        {
+                            name: 'title',
+                            type: 'string',
+                            required: true,
+                        },
+                        {
+                            name: 'slug',
+                            type: 'slug',
+                        },
+                        {
+                            name: 'markdown_content',
+                            type: 'markdown',
+                        },
+                    ],
+                },
+            ],
+        }),
+    ],
 });
