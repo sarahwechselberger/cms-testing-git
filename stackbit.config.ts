8ebc36ac66297a4c56098b258ac4da2d5cecdfb8
sarahwechselberger <sarah.wechselberger@wh-d.at>
2026-09-15 11:00:54 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -22,5 +22,13 @@ export default defineStackbitConfig({
                 }
             ]
         })
-    ]
+    ],
+    siteMap: ({ documents }) => {
+        return documents.map((document) => ({
+            stableId: document.id,
+            urlPath: `/${document.fields.slug ?? document.id}`,
+            document,
+            isHomePage: false
+        }));
+    }
 });
